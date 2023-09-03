75c324d014705cf7b4f28af27a93fc0255a5dab7
JosephDoUrden <yusufhan.sacak@bahcesehir.edu.tr>
2023-09-03 17:01:43 +0300

    get book from db by id

--- backend/index.js
+++ backend/index.js
@@ -45,7 +45,23 @@ app.post("/books", async (request, response) => {
 app.get("/books", async (request, response) => {
   try {
     const books = await Book.find({});
-    return response.status(200).json(books);
+    return response.status(200).json({
+      count: books.lenght,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Route for get books from db by id
+app.get("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    const book = await Book.findById(id);
+    return response.status(200).json(book);
   } catch (error) {
     console.log(error.message);
     response.status(500).send({ message: error.message });
