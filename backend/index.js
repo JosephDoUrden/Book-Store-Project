ff6dee19d8bbaa3e175b1170bba8cc3ea8e225e6
JosephDoUrden <yusufhan.sacak@bahcesehir.edu.tr>
2023-09-03 17:16:20 +0300

    delete book by id

--- backend/index.js
+++ backend/index.js
@@ -95,6 +95,23 @@ app.put("/books/:id", async (request, response) => {
   }
 });
 
+// Route for delete a book
+app.delete("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    const result = await Book.findByIdAndDelete(id);
+
+    if (!result) {
+      return response.status(404).json({ message: "Book not found!" });
+    }
+    return response.status(200).json({ message: "Book deleted successfully!" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
