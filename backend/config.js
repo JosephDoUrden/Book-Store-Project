f8ad16f63849ded12e0ee443c8762b3ca47e94d7
JosephDoUrden <yusufhan.sacak@bahcesehir.edu.tr>
2023-09-03 16:13:02 +0300

    db connection done

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,5 @@
+import "dotenv/config";
+
 export const PORT = 5555;
+
+export const mongoDBURL = process.env.mongoDBURL;
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,23 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome to MERN Stack Book Shop");
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to db");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
