7c17deb43f4e0452559a54b59856b92d2c9c2d95
JosephDoUrden <yusufhan.sacak@bahcesehir.edu.tr>
2023-09-06 00:00:14 +0300

    ADDED: Edit Book in React

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
 import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
