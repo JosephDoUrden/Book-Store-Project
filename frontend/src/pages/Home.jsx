8e6ade35bb744aa9f101a3704d861ac2056375aa
JosephDoUrden <yusufhan.sacak@bahcesehir.edu.tr>
2023-09-06 00:13:53 +0300

    ADDED: Delete Book in React

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -71,7 +71,7 @@ const Home = () => {
                     <Link to={`/books/edit/${book._id}`}>
                       <AiOutlineEdit className="text-2xl text-yellow-600" />
                     </Link>
-                    <Link to={`/books/delete/${`book._id`}`}>
+                    <Link to={`/books/delete/${book._id}`}>
                       <MdOutlineDelete className="text-2xl text-red-600" />
                     </Link>
                   </div>
